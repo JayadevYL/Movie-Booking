8d35e60b91c1ee2226bf3e2c3a31529bdea24164
Jayadev YL Gowda <jayadevylgowda10@gmail.com>
2025-02-08 14:56:23 +0530

    init commit
    Signed-off-by: Jayadev YL Gowda <jayadevylgowda10@gmail.com>

---
+++ src/main/java/com/MovieBooking/MovieBooking/service/UpdateCustomerDetails.java
@@ -0,0 +1,7 @@
+package com.MovieBooking.MovieBooking.service;
+
+import com.MovieBooking.MovieBooking.model.UserDetails;
+
+public interface UpdateCustomerDetails {
+    void updatePassword(UserDetails password);
+}
